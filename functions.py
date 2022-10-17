f7344652a4f6996777d970ad5d6cae8ac2b93f57
Gennadiy <renardes@gmail.com>
2022-10-17 10:41:00 +0500

    файловая структура проекта, глобальные переменные

--- functions.py
+++ functions.py
@@ -1,24 +1,11 @@
+"""Дополнительный модуль: вспомогательные функции."""
+
 import configparser
 
-# вариант решения 1
-
-def read_ini (file_name) -> dict:
-    """Читает ini-файл и возвращает его содержимое в формате вложенного словаря."""
-
-    config = configparser.ConfigParser()
-    config.read(file_name)
-    new_format = config.__dict__['_sections'].copy()
-    return new_format
-
-file_name='players.ini'
-res = read_ini(file_name)
-print(f'{res}')
 
 # вариант решения 2
-
-def read_ini (file_name) -> dict:
+def read_ini(file_name) -> dict:
     """Читает ini-файл и возвращает его содержимое в формате вложенного словаря."""
-
     config = configparser.ConfigParser()
     config.read(file_name)
     dictionary = {}
@@ -28,7 +15,10 @@ def read_ini (file_name) -> dict:
             dictionary[section][option] = config.get(section, option)
     return dictionary
 
+
+# СДЕЛАТЬ: изучите структуру файлов и данных в референсе, делайте по аналогии
+
+
 file_name = 'players.ini'
 res = read_ini(file_name)
 print(f'{res}')
-
