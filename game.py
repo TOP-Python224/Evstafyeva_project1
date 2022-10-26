df3e8a4948c9fdb7b27c19db9e3c0e3f83c80455
Gennadiy <renardes@gmail.com>
2022-10-26 19:22:36 +0500

    get_player_name(), draw_board(), human_turn() и check_win()

--- game.py
+++ game.py
@@ -6,7 +6,9 @@ import data
 
 def human_turn(training: bool = False) -> int:
     """Запрашивает у игрока и возвращает корректную координату ячейки поля для текущего хода."""
+    # ИСПРАВИТЬ: если пользователь введёт не цифровые символы, то на этом игра прервётся — вы должны предусмотреть такую ситуацию или хотя бы слушать преподавателя, когда он об этом говорит
     human_step = int(input('Введите координату своего хода от 1 до 9: '))
+    # ОТВЕТИТЬ: какой вами ожидается результат данного действия?
     human_step = data.BOARD[human_step-1]
     return human_step
 
@@ -25,14 +27,19 @@ def check_win() -> bool:
                             [2, 5, 8],
                             [0, 4, 8],
                             [2, 4, 6]]
-    for i in winning_combinations:
-        if (data.BOARD[i[0]] == data.TOKENS[0] and
-            data.BOARD[i[1]] == data.TOKENS[0] and
-            data.BOARD[i[2]] == data.TOKENS[0]
+    # ИСПОЛЬЗОВАТЬ: вы перебираете не индексы — а имя i используется для индексов
+    for combination in winning_combinations:
+        # ИСПОЛЬЗОВАТЬ: распаковку и алгебру логики
+        # i, j, k = combination
+        # if data.BOARD[i] == data.BOARD[j] == data.BOARD[k] != '':
+        #     ...
+        if (data.BOARD[combination[0]] == data.TOKENS[0] and
+            data.BOARD[combination[1]] == data.TOKENS[0] and
+            data.BOARD[combination[2]] == data.TOKENS[0]
             or
-            data.BOARD[i[0]] == data.TOKENS[1] and
-            data.BOARD[i[1]] == data.TOKENS[1] and
-            data.BOARD[i[2]] == data.TOKENS[1]
+            data.BOARD[combination[0]] == data.TOKENS[1] and
+            data.BOARD[combination[1]] == data.TOKENS[1] and
+            data.BOARD[combination[2]] == data.TOKENS[1]
         ):
             return True
         else:
